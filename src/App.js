1ae74379ec9686bb241a03522fc53ac3c2948db0
Arun Kumar <arunkumar413@gmail.com>
2023-05-08 14:53:13 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./styles.css";
+import { setEmail } from "./stateManagement/userSlice";
+
+
 
 export default function App() {
+
+  const {email} = useSelector((state) => state.user.email)
+  const dispatch = useDispatch()
+
+
+  function handleEmailChange(evt){
+
+    dispatch(setEmail(evt.target.value))
+  }
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <input value={email} onChange={handleEmailChange} name='name'/>
+      <h2> {email}</h2>
     </div>
   );
 }
